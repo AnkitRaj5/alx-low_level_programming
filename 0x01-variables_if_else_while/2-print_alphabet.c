6e887cbeb0b17abfc0c132f16d7f4d0c2aa2e621
AnkitRaj5 <ankitraj6241@gmail.com>
2022-09-09 12:13:21 -0700

    2 fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * main - main block
  * Description: Print aplhabet in lowercase
+ * Return: 0
  */
 int main(void)
 {
 	char c = 'a';
+
 	while (c <= 'z')
 	{
 		putchar(c);
