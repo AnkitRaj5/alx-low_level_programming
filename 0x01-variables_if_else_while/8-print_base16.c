0dd9b2ab62b7622b13a808918a907f3cb05ba280
AnkitRaj5 <ankitraj6241@gmail.com>
2022-09-09 12:54:15 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: print all the numbers of base 16
+ * Return: 0
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+	char c = 'a';
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
