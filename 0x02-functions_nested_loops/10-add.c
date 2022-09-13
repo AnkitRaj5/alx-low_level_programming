51b4c237a3834bad59143b68aed494b23729c5d7
Ankit Raj <ankitraj6241@gmail.com>
2022-09-14 00:43:42 +0530

    fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * add - return the sum of it's parameter
- * @a: int type
- * @b: int type 
- * Return: 0
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0
  */
+
+
 int add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
